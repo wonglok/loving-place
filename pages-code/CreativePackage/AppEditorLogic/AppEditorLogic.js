568f4931ac819b7cef82dc0a0de2cb2ef32c4bc4
wonglok <yellowhappy831@gmail.com>
2021-05-16 18:16:04 +0800

    UI

--- pages-code/CreativePackage/AppEditorLogic/AppEditorLogic.js
+++ pages-code/CreativePackage/AppEditorLogic/AppEditorLogic.js
@@ -1,17 +1,37 @@
 import { getID, Hand, ProjectStore } from "../AppEditorState/AppEditorState";
 
-export const addBlockerTemp = () => {
-  Hand.addMode = "addBlocker";
-};
-
 export const addBlocker = ({ point }) => {
-  Hand.floor = { x: 0, y: 0, z: 0 };
-
   let newObj = {
     _id: getID(),
     position: [point.x, point.y, point.z],
     title: "",
   };
 
+  let makePort = (type = "input") => {
+    return {
+      _id: getID(),
+      type,
+      blockerID: newObj._id,
+    };
+  };
+
   ProjectStore.blockers.addItem(newObj);
+
+  ProjectStore.ports.addItem(makePort("input"));
+  ProjectStore.ports.addItem(makePort("input"));
+  ProjectStore.ports.addItem(makePort("input"));
+  ProjectStore.ports.addItem(makePort("input"));
+  ProjectStore.ports.addItem(makePort("input"));
+
+  ProjectStore.ports.addItem(makePort("output"));
+  ProjectStore.ports.addItem(makePort("output"));
+  ProjectStore.ports.addItem(makePort("output"));
+  ProjectStore.ports.addItem(makePort("output"));
+  ProjectStore.ports.addItem(makePort("output"));
+};
+
+export const addConnection = () => {
+  //
+
+  console.log(123);
 };
